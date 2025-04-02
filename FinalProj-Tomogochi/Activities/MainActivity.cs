c5a11b0298204b1a8158743965e67809afcabd6d
Maxim Zyuryaev <zrv.maxim@gmail.com>
2025-04-02 14:13:44 +0300

    added a working demo for charting blood sugar and loaded it using fragments

--- FinalProj-Tomogochi/Activities/MainActivity.cs
+++ FinalProj-Tomogochi/Activities/MainActivity.cs
@@ -1,9 +1,12 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using AndroidX.AppCompat.App;
+using FinalProj_Tomogochi.Fragments;
 using Google.Android.Material.BottomNavigation;
 
 namespace FinalProj_Tomogochi.Activities
@@ -11,8 +14,7 @@ namespace FinalProj_Tomogochi.Activities
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
     public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
     {
-        TextView textMessage;
-
+        [System.Obsolete]
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -21,26 +23,50 @@ namespace FinalProj_Tomogochi.Activities
 
             BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
             navigation.SetOnNavigationItemSelectedListener(this);
+
+            navigation.SelectedItemId = Resource.Id.navigation_character;
+
+            LoadFragment(new CharacterFragment());
         }
+
+        [Obsolete]
+        private void LoadFragment(AndroidX.Fragment.App.Fragment fragment)
+        {
+            SupportFragmentManager.BeginTransaction()
+               .Replace(Resource.Id.fragment_container, fragment)
+               .Commit();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        [Obsolete]
         public bool OnNavigationItemSelected(IMenuItem item)
         {
+
+            AndroidX.Fragment.App.Fragment selectedFragment = null;
+
             switch (item.ItemId)
             {
-                case Resource.Id.navigation_home:
-                    Toast.MakeText(Application.Context, Resource.String.title_home, ToastLength.Long).Show();
-                    return true;
+                case Resource.Id.navigation_character:
+                    selectedFragment = new CharacterFragment();
+                    break;
                 case Resource.Id.navigation_dashboard:
                     Toast.MakeText(Application.Context, Resource.String.title_dashboard, ToastLength.Long).Show();
                     return true;
                 case Resource.Id.navigation_notifications:
                     Toast.MakeText(Application.Context, Resource.String.title_notifications, ToastLength.Long).Show();
                     return true;
+            }
+
+            if (selectedFragment != null)
+            {
+                LoadFragment(selectedFragment);
+                return true;
             }
             return false;
         }
---
+++ FinalProj-Tomogochi/Fragments/CharacterFragment.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Microcharts;
+using Microcharts.Droid;
+using SkiaSharp;
+using AndroidX.Fragment.App;
+
+namespace FinalProj_Tomogochi.Fragments
+{
+    [Obsolete]
+    public class CharacterFragment : Fragment
+	{
+        
+        [Obsolete]
+        public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+            View view = inflater.Inflate(Resource.Layout.character_view_screen, container, false);
+            var chartView = view.FindViewById<ChartView>(Resource.Id.chartView);
+
+            // Create sample data
+            var entries = new List<ChartEntry>();
+            var rnd = new Random();
+            int sugar = 120;
+            for (int i = 0; i < 15; i++)
+            {
+                string color = GetColorString(sugar);
+                entries.Add(new ChartEntry(sugar)
+                {
+                    Label = DateTime.Now.Add(TimeSpan.FromMinutes(i)).ToString("HH:mm"),
+                    ValueLabel = sugar.ToString(),
+                    Color = SKColor.Parse(color)
+                });
+                sugar += rnd.Next(-30, 30);
+            }
+
+            var chart = new LineChart
+            {
+                Entries = entries,
+                BackgroundColor = SKColor.Parse("#FFFFFF"),
+                LineMode = LineMode.Spline,
+                LineSize = 5,
+                LabelTextSize = 30,
+                PointMode = PointMode.Circle,
+                PointSize = 10,
+                ValueLabelOrientation = Microcharts.Orientation.Horizontal,
+            };
+
+            chartView.Chart = chart;
+
+            return view;
+        }
+
+        private string GetColorString(int sugar)
+        {
+            if (sugar > 70 && sugar < 180)
+                return "ADFF2F"; // Green
+            if (sugar >= 180)
+                return "FFEF00"; // Yellow
+            return "ED1B24"; // Red
+        }
+    }
+}
+
